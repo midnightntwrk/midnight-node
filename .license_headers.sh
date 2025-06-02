ba641cfad9d96582a866f3af1078e649b759afde
Giles Cope <gilescope@gmail.com>
2025-06-02 20:32:20 +0100

    feat: add sh licensing

--- .license_headers.sh
+++ .license_headers.sh
@@ -1,21 +1,31 @@
 find . -type f -name "*.rs" \( -path "*/src/*" -o -path "*/tests/*" \) | while IFS= read -r file; do
     if ! grep -q "SPDX-License-Identifier" "$file"; then
         tmpfile=$(mktemp)
-        cat .midnight.txt >> $tmpfile
+        sed 's/^/\/\/ /' .midnight.txt >> "$tmpfile"
         echo "" >> $tmpfile
         cat $file >> $tmpfile
         mv $tmpfile $file
     fi
 done
-
 
 find . -type f -name "*.js" \( -path "*/src/*" -o -path "*/tests/*" \) | while IFS= read -r file; do
     if ! grep -q "SPDX-License-Identifier" "$file"; then
         tmpfile=$(mktemp)
-        cat .midnight.txt >> $tmpfile
+        sed 's/^/\/\/ /' .midnight.txt >> "$tmpfile"
         echo "" >> $tmpfile
         cat $file >> $tmpfile
         mv $tmpfile $file
     fi
 done
 
+find . -type f -name "*.sh" | while IFS= read -r file; do
+    if ! grep -q "SPDX-License-Identifier" "$file"; then
+        tmpfile=$(mktemp)
+        echo "#!/usr/bin/env bash" >> $tmpfile
+        echo "" >> $tmpfile
+        sed 's/^/# /' .midnight.txt >> "$tmpfile"
+        echo "" >> $tmpfile
+        cat $file >> $tmpfile
+        mv $tmpfile $file
+    fi
+done
--- local-environment/setup.sh
+++ local-environment/setup.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# This file is part of midnight-node.
+# Copyright (C) 2025 Midnight Foundation
+# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/env bash
+
 MIDNIGHT_NODE_IMAGE="ghcr.io/midnight-ntwrk/midnight-node:0.12.0-rc.3"
 CARDANO_IMAGE="ghcr.io/intersectmbo/cardano-node:10.1.4"
 DBSYNC_IMAGE="ghcr.io/intersectmbo/cardano-db-sync:13.6.0.4"
