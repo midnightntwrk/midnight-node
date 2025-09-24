490fd6029ea5ee6e6b6ce7916b5f61f800fc0747
Oscar Bailey <79094698+ozgb@users.noreply.github.com>
2025-09-24 17:17:03 +0100

    test: add subxt and block sub example to e2e tests

--- tests/e2e/tests/create_wallet.rs
+++ tests/e2e/tests/create_wallet.rs
@@ -2,16 +2,15 @@
 mod config;
 use midnight_node_e2e::api::cardano::*;
 use midnight_node_e2e::api::midnight::*;
+use ogmios_client::query_ledger_state::QueryLedgerState;
 use whisky::Asset;
-use ogmios_client::{query_ledger_state::QueryLedgerState};
-
 
 #[tokio::test]
 async fn register_for_dust_production() {
 	let cardano_wallet = create_wallet();
 	let bech32_address = get_wallet_address(&cardano_wallet);
 	println!("New Cardano wallet created: {:?}", bech32_address);
-	
+
 	let dust_hex = new_dust_hex(32);
 	println!("Registering Cardano wallet {} with DUST address {}", bech32_address, dust_hex);
 
@@ -24,4 +23,5 @@ async fn register_for_dust_production() {
 	assert_eq!(utxos.len(), 2, "New wallet should have exactly two UTXOs after funding");
 
 	register(&bech32_address, &dust_hex, &cardano_wallet, &tx_in, &collateral_utxo).await;
+	subscribe_to_blocks().await.expect("failed to listen to blocks");
 }
