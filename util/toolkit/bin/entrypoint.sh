f9f76028fcfd9775301eceea6bc34805ef4583c5
Simon Gellis <simongellis@gmail.com>
2025-10-14 16:07:40 -0400

    fix: chown the out dir at runtime

--- util/toolkit/bin/entrypoint.sh
+++ util/toolkit/bin/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /tmp /mnt/output
-chown -R appuser:appuser /tmp /mnt/output
+mkdir -p /tmp /mnt/output /out
+chown -R appuser:appuser /tmp /mnt/output /out
 
 runuser -u appuser /midnight-node-toolkit -- "$@"
