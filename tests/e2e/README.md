247b5492a7120d6d2a0c8b90e0693bc9c40c5c79
Radosław Sporny <404@rspo.dev>
2025-10-20 16:22:07 +0200

    Add: e2e tests in Rust (#63)
    * add: rust e2e tests framework
    
    ---------
    
    Co-authored-by: Oscar Bailey <79094698+ozgb@users.noreply.github.com>

---
+++ tests/e2e/README.md
@@ -0,0 +1,5 @@
+# Integration Tests
+
+These tests are not run by default when running `cargo test` in the workspace.
+
+To execute these tests, run `cargo test --test e2e_tests`
---
+++ tests/e2e/src/api/midnight.rs
@@ -0,0 +1,70 @@
+use crate::cfg::load_config;
+use midnight_node_metadata::midnight_metadata_latest::{
+	self as mn_meta,
+	native_token_observation::{self},
+};
+use rand::RngCore;
+use subxt::{blocks::ExtrinsicEvents, OnlineClient, SubstrateConfig};
+
+pub fn new_dust_hex(bytes: usize) -> String {
+	let mut a = vec![0u8; bytes];
+	rand::rng().fill_bytes(&mut a);
+	a.iter().map(|b| format!("{:02x}", b)).collect::<String>()
+}
+
+pub async fn subscribe_to_cngd_registration_extrinsic(
+	tx_id: &[u8],
+) -> Result<ExtrinsicEvents<SubstrateConfig>, Box<dyn std::error::Error>> {
+	println!("Subscribing for registration extrinsic with tx_id: 0x{}", hex::encode(tx_id));
+	let url = load_config().node_url;
+	let api = OnlineClient::<SubstrateConfig>::from_url(&url).await?;
+
+	let mut blocks_sub = api.blocks().subscribe_finalized().await?;
+
+	use tokio::time::{timeout, Duration};
+	let result = timeout(Duration::from_secs(60), async {
+		while let Some(block) = blocks_sub.next().await {
+			let block = block?;
+			let block_number = block.header().number;
+			println!("Block #{block_number}:");
+
+			let extrinsics = block.extrinsics().await?;
+			for ext in extrinsics.iter() {
+				let events = ext.events().await?;
+
+				let decoded_ext = ext.as_root_extrinsic::<mn_meta::Call>();
+				let runtime_call = decoded_ext.unwrap();
+				match &runtime_call {
+					mn_meta::Call::NativeTokenObservation(e) => if let native_token_observation::Call::process_tokens { utxos, .. } = e {
+							println!(
+								"  NativeTokenObservation::process_tokens called with {} UTXOs",
+								utxos.len()
+							);
+							if utxos.is_empty() {
+								continue;
+							} else {
+								for utxo in utxos {
+									let utxo_tx_id = utxo.header.tx_hash.0;
+									if utxo_tx_id == tx_id {
+										println!("*** Found UTXO with matching registration tx hash: 0x{} ***", hex::encode(tx_id));
+										return Ok(events);
+									} else {
+										println!("Tx hash 0x{} does not match expected registration tx hash 0x{}", hex::encode(utxo_tx_id), hex::encode(tx_id));
+									}
+								}
+							}
+						}
+					_ => {
+						continue;
+					},
+				}
+			}
+		}
+		Err("Did not find registration event".into())
+	}).await;
+
+	match result {
+		Ok(res) => res,
+		Err(_) => Err("Timeout waiting for registration event".into()),
+	}
+}
