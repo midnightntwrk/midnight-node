bcc8a637e9d8145b753f019d0d491e633fcbe3ef
B. Yap <2826165+b-yap@users.noreply.github.com>
2025-12-29 13:58:44 +0800

    fix cargo fmt
    Signed-off-by: B. Yap <2826165+b-yap@users.noreply.github.com>

--- util/toolkit/src/commands/generate_genesis.rs
+++ util/toolkit/src/commands/generate_genesis.rs
@@ -63,7 +63,7 @@ pub async fn execute(
 		.iter()
 		.map(|(_k, v)| {
 			let wallet_seed_str = v.as_str().ok_or("seeds file object value was not a string")?;
-			let wallet_seed= WalletSeed::try_from_hex_str(wallet_seed_str)?;
+			let wallet_seed = WalletSeed::try_from_hex_str(wallet_seed_str)?;
 			Ok(wallet_seed)
 		})
 		.collect();
