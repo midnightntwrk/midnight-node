c9d81937d5b745c9d8157896cbc8cc2ab920ad39
Giles Cope <gilescope@gmail.com>
2026-01-09 12:26:57 +0000

    feat: remove incorrect references to minting NIGHT

---
+++ changes/changed/remove-block-reward-minting.md
@@ -0,0 +1,13 @@
+#ledger
+# Remove block reward coin minting functionality
+
+Removed the `mint_coins` functionality from the ledger bridge API and the midnight pallet's block reward minting logic.
+This removes the ability to mint coins as block rewards through the ledger's mint_coins API.
+This will later be reworked as no NIGHT is being minted when block rewards are paid out.
+
+Changes include:
+- Removed `mint_coins` method from `LedgerBridge` trait
+- Removed block reward minting logic from midnight pallet's `on_finalize` hook
+
+PR: <link to PR>
+JIRA: https://shielded.atlassian.net/browse/PM-21159
--- ledger/src/host_api/mod.rs
+++ ledger/src/host_api/mod.rs
@@ -209,25 +209,6 @@ pub trait LedgerBridge {
 	}
 
 	/*
-	 * Mints system coins for block rewards
-	 */
-	// Current Enabled Version
-	fn mint_coins(
-		&mut self,
-		state_key: PassFatPointerAndRead<&[u8]>,
-		amount: PassFatPointerAndDecode<u128>,
-		receiver: PassFatPointerAndRead<&[u8]>,
-		block_context: PassFatPointerAndDecode<BlockContext>,
-	) -> AllocateAndReturnByCodec<Result<Vec<u8>, latest::types::LedgerApiError>> {
-		latest::Bridge::<Signature, Database>::mint_coins(
-			state_key,
-			amount,
-			receiver,
-			block_context,
-		)
-	}
-
-	/*
 	 * Returns the unclaimed amount for a provided beneficiary address
 	 */
 	// Current Enabled Version
@@ -482,21 +463,21 @@ pub trait LedgerBridgeHf {
 		)
 	}
 
-	// Hard-fork Version
-	fn mint_coins(
-		&mut self,
-		state_key: PassFatPointerAndRead<&[u8]>,
-		amount: PassFatPointerAndDecode<u128>, //TODO can we be more efficient?
-		receiver: PassFatPointerAndRead<&[u8]>,
-		block_context: PassFatPointerAndDecode<BlockContext>,
-	) -> AllocateAndReturnByCodec<Result<Vec<u8>, hard_fork_test::types::LedgerApiError>> {
-		hard_fork_test::Bridge::<SignatureHF, DatabaseHF>::mint_coins(
-			state_key,
-			amount,
-			receiver,
-			block_context,
-		)
-	}
+	// // Hard-fork Version
+	// fn mint_coins(
+	// 	&mut self,
+	// 	state_key: PassFatPointerAndRead<&[u8]>,
+	// 	amount: PassFatPointerAndDecode<u128>, //TODO can we be more efficient?
+	// 	receiver: PassFatPointerAndRead<&[u8]>,
+	// 	block_context: PassFatPointerAndDecode<BlockContext>,
+	// ) -> AllocateAndReturnByCodec<Result<Vec<u8>, hard_fork_test::types::LedgerApiError>> {
+	// 	hard_fork_test::Bridge::<SignatureHF, DatabaseHF>::mint_coins(
+	// 		state_key,
+	// 		amount,
+	// 		receiver,
+	// 		block_context,
+	// 	)
+	// }
 
 	// Hard-fork Version
 	fn get_unclaimed_amount(
