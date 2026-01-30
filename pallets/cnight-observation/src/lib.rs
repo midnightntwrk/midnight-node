6f942ea2deb33de1943d6165b83f44f17dffc4a2
Michał Skowron <michal.skowron@iohk.io>
2026-01-30 17:06:54 +0100

    fix: prevent duplicate inherent execution within same block

--- pallets/cnight-observation/src/lib.rs
+++ pallets/cnight-observation/src/lib.rs
@@ -150,6 +150,8 @@ pub mod pallet {
 		MaxCardanoAddrLengthExceeded,
 		MaxRegistrationsExceeded,
 		LedgerApiError(LedgerApiError),
+		/// Only one inherent is allowed per block
+		InherentAlreadyExecuted,
 	}
 
 	impl<T: Config> From<LedgerApiError> for Error<T> {
@@ -217,6 +219,9 @@ pub mod pallet {
 	/// Max amount of Cardano transactions that can be processed per block
 	pub type CardanoTxCapacityPerBlock<T: Config> =
 		StorageValue<_, u32, ValueQuery, DefaultCardanoTxCapacityPerBlock>;
+
+	#[pallet::storage]
+	pub type InherentExecutedThisBlock<T: Config> = StorageValue<_, bool, ValueQuery>;
 
 	#[pallet::genesis_config]
 	#[derive(frame_support::DefaultNoBound)]
@@ -582,6 +587,13 @@ pub mod pallet {
 		}
 	}
 
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_finalize(_n: BlockNumberFor<T>) {
+			InherentExecutedThisBlock::<T>::kill();
+		}
+	}
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::call_index(0)]
@@ -592,6 +604,8 @@ pub mod pallet {
 			next_cardano_position: CardanoPosition,
 		) -> DispatchResult {
 			ensure_none(origin)?;
+			ensure!(!InherentExecutedThisBlock::<T>::get(), Error::<T>::InherentAlreadyExecuted);
+			InherentExecutedThisBlock::<T>::put(true);
 
 			let mut events: Vec<CNightGeneratesDustEventSerialized> = Vec::new();
 
