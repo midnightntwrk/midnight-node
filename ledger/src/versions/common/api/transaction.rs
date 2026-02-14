ec26a79e4fbec292343ceccdaf9de680d5aa59e5
Giles Cope <gilescope@gmail.com>
2026-02-14 12:16:12 +0000

    fix: keep existing preview ordering
    Signed-off-by: Giles Cope <gilescope@gmail.com>

--- ledger/src/versions/common/api/transaction.rs
+++ ledger/src/versions/common/api/transaction.rs
@@ -142,7 +142,9 @@ impl UnshieldedUtxos {
 	}
 
 	pub fn inputs(&self) -> Vec<UtxoInfo> {
-		self.inputs.values().flat_map(|utxos| utxos.iter()).cloned().collect()
+		// TODO: this rev() is only required to match preview.
+		// We could drop it before mainnet as a breaking change.
+		self.inputs.values().rev().flat_map(|utxos| utxos.iter()).cloned().collect()
 	}
 
 	pub fn outputs(&self) -> Vec<UtxoInfo> {
