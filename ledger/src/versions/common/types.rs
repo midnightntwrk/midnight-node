8b8f6c41595e6c7c84e9699433cd7b7732c3157e
Giles Cope <gilescope@gmail.com>
2026-02-23 08:30:35 +0000

    chore: fix clippy
    Signed-off-by: Giles Cope <gilescope@gmail.com>

--- ledger/src/versions/common/types.rs
+++ ledger/src/versions/common/types.rs
@@ -436,10 +436,9 @@ mod tests {
 			for n in 0..256u32.pow(depth) {
 				let bytes: Vec<u8> = (0..depth).map(|i| ((n >> (8 * i)) & 0xFF) as u8).collect();
 				let mut slice: &[u8] = &bytes;
-				if let Ok(e) = LedgerApiError::decode(&mut slice) {
-					if slice.is_empty() {
-						result.push(e);
-					}
+				if let Ok(e) = LedgerApiError::decode(&mut slice)
+					&& slice.is_empty() {
+					result.push(e);
 				}
 			}
 		}
