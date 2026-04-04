701e6a2490ce454e7f00c0e7e6e857115b1e07d1
Giles Cope <gilescope@gmail.com>
2026-04-04 08:13:57 +0100

    chore: cargo fmt
    Signed-off-by: Giles Cope <gilescope@gmail.com>

--- node/src/main_chain_follower.rs
+++ node/src/main_chain_follower.rs
@@ -194,16 +194,16 @@ pub async fn create_cached_data_sources(
 	};
 
 	let candidates_pool = get_connection(
-    postgres_uri, 
-    CANDIDATES_POOL_CFG, 
-    cfg.allow_non_ssl,
-    cfg.ssl_root_cert.as_deref(),
-  )
-		.await
-		.map_err(|e| {
-			log::warn!("Failed to connect to database for candidates data source: {e}");
-			e
-		})?;
+		postgres_uri,
+		CANDIDATES_POOL_CFG,
+		cfg.allow_non_ssl,
+		cfg.ssl_root_cert.as_deref(),
+	)
+	.await
+	.map_err(|e| {
+		log::warn!("Failed to connect to database for candidates data source: {e}");
+		e
+	})?;
 
 	// All these pools are connections to the same database, so we can use any pool to create the index
 	create_index_if_not_exists(&candidates_pool).await;
@@ -227,11 +227,11 @@ pub async fn create_cached_data_sources(
 		cfg.allow_non_ssl,
 		cfg.ssl_root_cert.as_deref(),
 	)
-		.await
-		.map_err(|e| {
-			log::warn!("Failed to connect to database for sidechain data source: {e}");
-			e
-		})?;
+	.await
+	.map_err(|e| {
+		log::warn!("Failed to connect to database for sidechain data source: {e}");
+		e
+	})?;
 	let sidechain_block_data_source = Arc::new(BlockDataSourceImpl::from_config(
 		sidechain_pool,
 		db_sync_block_data_source_config.clone(),
@@ -248,11 +248,11 @@ pub async fn create_cached_data_sources(
 		cfg.allow_non_ssl,
 		cfg.ssl_root_cert.as_deref(),
 	)
-		.await
-		.map_err(|e| {
-			log::warn!("Failed to connect to database for mc_hash data source: {e}");
-			e
-		})?;
+	.await
+	.map_err(|e| {
+		log::warn!("Failed to connect to database for mc_hash data source: {e}");
+		e
+	})?;
 	let mc_hash_block_data_source = BlockDataSourceImpl::from_config(
 		mc_hash_pool,
 		db_sync_block_data_source_config.clone(),
@@ -267,11 +267,11 @@ pub async fn create_cached_data_sources(
 		cfg.allow_non_ssl,
 		cfg.ssl_root_cert.as_deref(),
 	)
-			.await
-			.map_err(|e| {
-				log::warn!("Failed to connect to database for cnight_observation data source: {e}");
-				e
-			})?;
+	.await
+	.map_err(|e| {
+		log::warn!("Failed to connect to database for cnight_observation data source: {e}");
+		e
+	})?;
 	let cnight_observation = MidnightCNightObservationDataSourceImpl::new(
 		cnight_observation_pool,
 		midnight_metrics_opt.clone(),
@@ -284,13 +284,13 @@ pub async fn create_cached_data_sources(
 		cfg.allow_non_ssl,
 		cfg.ssl_root_cert.as_deref(),
 	)
-			.await
-			.map_err(|e| {
-				log::warn!(
-					"Failed to connect to database for federated_authority_observation data source: {e}"
-				);
-				e
-			})?;
+	.await
+	.map_err(|e| {
+		log::warn!(
+			"Failed to connect to database for federated_authority_observation data source: {e}"
+		);
+		e
+	})?;
 	let federated_authority_observation = FederatedAuthorityObservationDataSourceImpl::new(
 		federated_authority_observation_pool,
 		midnight_metrics_opt,
@@ -303,11 +303,11 @@ pub async fn create_cached_data_sources(
 		cfg.allow_non_ssl,
 		cfg.ssl_root_cert.as_deref(),
 	)
-		.await
-		.map_err(|e| {
-			log::warn!("Failed to connect to database for bridge data source: {e}");
-			e
-		})?;
+	.await
+	.map_err(|e| {
+		log::warn!("Failed to connect to database for bridge data source: {e}");
+		e
+	})?;
 
 	let bridge = CachedTokenBridgeDataSourceImpl::new(
 		bridge_pool,
