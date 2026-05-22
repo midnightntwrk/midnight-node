c48752e6c537a7d18001870dca5f623985533a8b
Oscar Bailey <79094698+ozgb@users.noreply.github.com>
2026-05-22 14:57:52 +0100

    fix: revert local-env changes
    Signed-off-by: Oscar Bailey <79094698+ozgb@users.noreply.github.com>

--- local-environment/src/networks/local-env/configurations/midnight-nodes/midnight-node-4/entrypoint.sh
+++ local-environment/src/networks/local-env/configurations/midnight-nodes/midnight-node-4/entrypoint.sh
@@ -23,7 +23,8 @@ export STORAGE_SEPARATION=unified
 
 ./midnight-node \
   --chain=/shared/chain-spec.json \
-  --node-key=1000000000000000000000000000000000000000000000000000000000000004 \
+  --validator \
+  --node-key=0000000000000000000000000000000000000000000000000000000000000004 \
   --port=30336 \
   --public-addr=/dns/midnight-node-4/tcp/30336/p2p/12D3KooWSsChzF81YDUKpe9Uk5AHV5oqAaXAcWNSPYgoLauUk4st \
   --bootnodes="/dns/midnight-node-1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
