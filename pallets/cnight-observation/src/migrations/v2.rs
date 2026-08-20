df1ee5797bfc3275b9f00f7555d561f2242d4abf
Oscar Bailey <79094698+ozgb@users.noreply.github.com>
2026-08-20 13:18:56 +0100

    docs: fix license headers
    Signed-off-by: Oscar Bailey <79094698+ozgb@users.noreply.github.com>

--- pallets/cnight-observation/src/migrations/v2.rs
+++ pallets/cnight-observation/src/migrations/v2.rs
@@ -1,3 +1,4 @@
+// This file is part of midnight-node.
 // Copyright (C) Midnight Foundation
 // SPDX-License-Identifier: Apache-2.0
 // Licensed under the Apache License, Version 2.0 (the "License");
