60a98d5ea91bca0493c92d73ef3c69df74c8f836
Lech Głowiak <lech.glowiak@shielded.io>
2026-09-08 14:59:00 +0200

    fixup d412d621de3bc61951b81d819ab3d170b33c57c2 - remove V1ToV2 migration, prerequisite is to run from v3 version
    Signed-off-by: Lech Głowiak <lech.glowiak@shielded.io>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -379,8 +379,6 @@ impl frame_system::Config for Runtime {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 	type RuntimeTask = RuntimeTask;
 	type SingleBlockMigrations = (
-		// Initializes the QueuedCommittee storage added in v2
-		pallet_session_validator_management::migrations::v2::V1ToV2Migration<Runtime>,
 		// Add BABE keys
 		crate::migrations::authority_keys::AddBabeToSessionKeysMigration,
 	);
