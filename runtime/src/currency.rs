e4ea759a913be90795c5b1a04698dd03a0af75ed
Tomasz Bartos <tomasz.bartos@shielded.io>
2026-09-09 11:05:56 +0200

    refactor(node): read committee membership from Aura/Babe APIs instead of SessionKeys storage
    Signed-off-by: Tomasz Bartos <tomasz.bartos@shielded.io>

--- runtime/src/currency.rs
+++ runtime/src/currency.rs
@@ -15,8 +15,6 @@ use core::ops::Div;
 use frame_support::{
 	dispatch::DispatchResult,
 	traits::{
-		BalanceStatus, Currency, ExistenceRequirement, Imbalance, ReservableCurrency, SameOrOther,
-		SignedImbalance, TryDrop, WithdrawReasons,
 		fungible::{
 			Inspect as FungibleInspect, InspectHold as FungibleInspectHold,
 			Mutate as FungibleMutate, MutateHold as FungibleMutateHold,
@@ -26,9 +24,11 @@ use frame_support::{
 		tokens::{
 			DepositConsequence, Fortitude, Precision, Preservation, Provenance, WithdrawConsequence,
 		},
+		BalanceStatus, Currency, ExistenceRequirement, Imbalance, ReservableCurrency, SameOrOther,
+		SignedImbalance, TryDrop, WithdrawReasons,
 	},
 };
-use sp_runtime::{DispatchError, traits::Saturating};
+use sp_runtime::{traits::Saturating, DispatchError};
 
 pub struct CurrencyWaiver;
 
